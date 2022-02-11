8807f72f35283de30833db20d0b2c3623c1185db
boodscode237 <65740750+boodscode237@users.noreply.github.com>
2022-02-12 00:20:41 +0500

    initial commit

--- src/main/java/com/boodscode/Main.java
+++ src/main/java/com/boodscode/Main.java
@@ -3,5 +3,6 @@ package com.boodscode;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
     }
 }
